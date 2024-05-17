c5570594f17752c974916ff153b32cfe1224829b
Captainbrooks <138337847+Captainbrooks@users.noreply.github.com>
2024-05-17 14:30:14 -0400

    Making a responsive navbar
    I deleted all the previous code of the navbar and Addded a bootstrap navbar along with logo

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,113 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import '../../styles/Navbar.css';
-// import logo from "../../images/restrologo.png";
-import logo from "../../images/logo-no-background.png";
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { FaShoppingCart } from 'react-icons/fa';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { selectUser, logoutUser, selectIsAdmin } from '../../store/features/userSlice';
-import { selectCart } from '../../store/features/cartSlice';
-
-
-
+import logo from "../../images/logo-no-background.png";
 
 function Navbar() {
-
-  const user = useSelector(selectUser);
-
-  const isAdmin=useSelector(selectIsAdmin);
-  const dispatch = useDispatch();
-
-
-
-
-  const cart=useSelector(selectCart);
-  const totalNumberOfItems = cart.reduce((total, item) => total + item.quantity, 0);
-
-  const location = useLocation();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
-  };
-  
   return (
+    <nav class="navbar navbar-expand-md bg-body-tertiary bg-dark">
+  <div class="container-md p-1 px-2">
+    <div className="d-flex justify-content-between align-items-center">
+  <Link to="/"><img src={logo} className='logo-img' alt="" /></Link>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
   
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark custom-navbar">
-      <Link to="/"><img src={logo} className='logo-img' alt="" /></Link>
-      <button className="navbar-toggler" type="button" onClick={toggleNavbar} data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded={isOpen} aria-label="Toggle navigation">
-        {isOpen ? <FontAwesomeIcon className='toggle-icons' icon={faTimes} size='lg' /> : <FontAwesomeIcon className='toggle-icons' icon={faBars} size='lg' />} {/* Toggle between hamburger and X icons */}
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-
-          <li className={`nav-item dropdown ${location.pathname === '/menu' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/menu">
-              Menu
-            </Link>
-          </li>
-          <li className={`nav-item ${location.pathname === '/book-reservations' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/book-reservations">Book Reservation</Link>
-          </li>
-
-          <li className={`nav-item ${location.pathname === '/events' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/events">Events</Link>
-          </li>
-
-          <li className={`nav-item ${location.pathname === '/contact' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/contact">Contact Us</Link>
-          </li>
-
-        </ul>
-
-   
-
-        {user ? (<div className="order">
-          <button type="button" className="btn btn-warning" onClick={() => dispatch(logoutUser())}>Logout</button>
-        </div>) : (
-          ""
-        )
-
-        
-       
-
-        
-
-        }
-
-
-    
-        <div className="order">
-          {
-     !isAdmin ?   <Link to="/order-online"><button type="button" className="btn btn-warning">Order Online</button></Link>: <Link to="/admin-dashboard"><button type="button" className="btn btn-warning">Admin Dashboard</button></Link>
-          }
-
-
-        </div>
-        <div className="cart-logo">
-          {
-             (<Link to="/cart"><div className='count' style={{display:"flex",justifyContent:"center",padding:"5px", backgroundColor:"black",borderRadius:"50%",color:"white"}}> {totalNumberOfItems}</div><FaShoppingCart size="2em" className={`${location.pathname === '/cart' ? 'text-warning' : 'text-white'}`}/>
-           </Link>)
-          }
-        </div>
-
-      </div>
-      
-    </nav>
-  
-       
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav  m mb-2 mb-lg-0 text-end pe-2 ">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
   );
 }
 
 export default Navbar;
-
-
-
-
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,11 +10,11 @@ function Home() {
   return (
   <div className="home-page">
      <Navbar/>
-    <Welcome/>
+    {/* <Welcome/>
     <Foods/>
     <About/>
     <Ratings/>
-    <Footer/>
+    <Footer/> */}
   </div>
   )
 }
