a979e087eb11db7cc37a2776afa4ab440057d981
Captainbrooks <138337847+Captainbrooks@users.noreply.github.com>
2024-05-19 00:49:12 -0400

    Responsive Navbar along with theme
    Made a responsive navbar  and manage to add a theme and put some hover affecting

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,39 +1,73 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+
+import React, { useEffect, useState } from 'react';
+import '../../styles/Navbar.css';
+// import logo from "../../images/restrologo.png";
 import logo from "../../images/logo-no-background.png";
+import { Link, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FaShoppingCart } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectUser, logoutUser, selectIsAdmin } from '../../store/features/userSlice';
+import { selectCart } from '../../store/features/cartSlice';
 
 function Navbar() {
+
+
+
+
+
+
+  const user = useSelector(selectUser);
+
+  const isAdmin=useSelector(selectIsAdmin);
+  const dispatch = useDispatch();
+
+  const cart=useSelector(selectCart);
+  const totalNumberOfItems = cart.reduce((total, item) => total + item.quantity, 0);
+
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  
+
   return (
-    <nav class="navbar navbar-expand-md bg-body-tertiary bg-dark">
-  <div class="container-md p-1 px-2">
-    <div className="d-flex justify-content-between align-items-center">
+    <nav className="navbar navbar-expand-md sticky-top bg-secondary">
+  <div className="container-md px-2 py-4">
+    <div className="d-flex justify-content-between align-items-center w-100">
   <Link to="/"><img src={logo} className='logo-img' alt="" /></Link>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+  <button className="navbar-toggler" type="button" onClick={toggleNavbar} data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isOpen} aria-label="Toggle navigation">
+
+       {isOpen ? <FontAwesomeIcon className='toggle-icons' icon={faTimes} size='lg' /> : <FontAwesomeIcon className='toggle-icons' icon={faBars} size='lg' />} {/* Toggle between hamburger and X icons */}
     </button>
   </div>
   
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav  m mb-2 mb-lg-0 text-end pe-2 ">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav m mb-2 mb-lg-0 text-end pe-2">
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/menu' ? 'active' : ''}`} to="/menu">Menu</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/book-reservations' ? 'active' : ''}`} to="/book-reservations">Book Reservation</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/events' ? 'active' : ''}`} to="/events">Events</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/contact' ? 'active' : ''}`} to="/contact">Contact Us</Link>
+            </li>
           </ul>
-        </li>
-      </ul>
-    </div>
+        </div>
   </div>
+
+
 </nav>
   );
 }
